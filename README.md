a3bddac0bb023393921ebe0f501f6d778df14f73
stern-shawn <stern.shawn@gmail.com>
2016-09-02 19:26:18 -0700

    Fleshing out the README file so people can understand the app outside of the FreeCodeCamp group. Updating script using linter feedback to properly space parens on function declarations, remove semicolons, use single-quotes for strings, and so on.

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# pomodoroTimer
+# Pomodoro Timer - FreeCodeCamp Project
+---
+## 1. What This Project Does
+The purpose of this app is to provide the user with a simple Pomodoro Timer, a productivity tool with the intent of working for 25 minutes at a time, broken up by 5 minute breaks before the next Pomodoro begins. Through building this app I got exposure to the JavaScript built-in Date and Math objects, and how to create and control timed actions using setInterval() and callbacks.
+
+The user stories for this project are:
+
+* The user can start a 25 minute pomodoro, and the timer will go off once 25 minutes has elapsed.
+* The user can reset the clock for their next pomodoro.
+* The user can customize the length of each pomodoro.
+* Use any libraries or APIs needed, and give it your own personal style.
+
+Since style and functionality other than the base user stories is up to me, I decided to add the following functionality:
+
+* At the end of a Pomodoro, after the user closes the alert, their break will begin. Once the break alert has been closed, the next Pomodoro will begin, and so on.
+
+The originally submitted version of the app fulfilled all user stories, and the various iterations since then have been improvements either in styling or in making the code more succinct.
+
+## 2. How To Set This Up
+If you want to clone this project for yourself, the process is very simple due to the structure of the project.
+
+1. Either manually download and unzip files to a location, or clone this repo through Git.
+2. Open the index.html file in your browser. This project is built on HTML5/CSS3/JavaScript/jQuery and the necessary resources from Bootstrap and FontAwesome are linked through the HTML file, so there is no need to install any packages through NPM.
+
+## 3. Project Goals
+As one of my early web development projects, this was a chance for me to:
+
+* Continue working with HTML/CSS and using Bootstrap for styling and creating a crisp, visually appealing user experience.
+* Practice using jQuery to monitor divs and buttons for clicks and apply appropriate responses to the DOM.
+* Get experience using JavaScript built-in objects such as the Math and Date objects.
+* Learn to create delayed/timed effects using the setInterval function.
+* Look for opportunities to use special JavaScript functions like .map, .every, etc to make code more pure and declarative vs imperative.
+* Continue producing well-documented code with repetitive functionality factored out into specific functions that minimize redundancy, keep code short, and increase readability/maintainability.
+
+Now that I'm coming back and reviewing past projects (such as this one), my new goals in addition to supporting the previous ones are:
+
+* Redesign the app to fit into my growing design language/styling through color, layout, animations, and reduction of unnecessary elements
+* Refactor any redundancies in my JavaScript and make old code compliant with current linters (such as AirBnB's standards)
+
+## 4. Link to Live Site
+The latest version of the site can be viewed [here](https://stern-shawn.github.io/FCC-PomodoroTimer/) thanks to gh-pages hosting.
+
+## 5. Roadmap
+TODO:
+
+* ~~Establish initial template in plain HTML/CSS, including a location for the timer text, buttons for start/break/reset, and buttons to increment/decrement the Pomodor length~~
+* ~~Apply Bootstrap styling to make the app more responsive on mobile devices and when the desktop window is resized~~
+* ~~Use Date object to get current time, Math object to correctly convert values (using floor function) to intervals of seconds, minutes, hours, etc. Use this and add the pomodoro to the time to find out the exected end times of pomodoros/breaks~~
+* ~~Figure out how to use the setInterval function to determine how much time is left between right now and the expected end of whatever pomodoro/break we are in once every second~~
+* ~~Allow the user to adjust the length of the Pomorodo using the +/- buttons. This can be done at any time and should update the display if a timer isn't currently running~~
+* Restyle app (Currently when unstacked, the +/- buttons are in a funny location maybe? Maybe it's just me)
+* Add animations
--- script.js
+++ script.js
@@ -1,121 +1,120 @@
 // Make these objects/functions global so it can be grabbed in any context
-var interval = null;
-var pomtime = 25;
-var timerRunning = false;
+var interval = null
+var pomtime = 25
+var timerRunning = false
 
-function getTimeRemaining(targetTime) {
+function getTimeRemaining (targetTime) {
   // Get the total time difference between the expected and current time
-  var time = Date.parse(targetTime) - Date.parse(new Date());
+  var time = Date.parse(targetTime) - Date.parse(new Date())
   // Partition result into finer units like minutes and seconds (no need to
   // track hours, etc)
   // Result is in milliseconds so we'll need to convert up from that...
   // Use Math.floor to round down to whole seconds/minutes
-  var seconds = Math.floor((time / 1000) % 60);
-  var minutes = Math.floor((time / 1000 / 60) % 60);
+  var seconds = Math.floor((time / 1000) % 60)
+  var minutes = Math.floor((time / 1000 / 60) % 60)
 
   // Since there are multiple parts to be returned, lets return an object
   return {
     'seconds': seconds,
     'minutes': minutes,
     'total': time
-  };
+  }
 }
 
-function startClock(targetTime, type) {
-  // var timer = $('#timerValue');
+function startClock (targetTime, type) {
+  // var timer = $('#timerValue')
   // Create the timer, re-evaluate every second
-  interval = setInterval(function() {
-    var t = getTimeRemaining(targetTime);
+  interval = setInterval(function () {
+    var t = getTimeRemaining(targetTime)
     // Add leading zero to keep digits at least two per type (mins/secs)
     // Use slice(-2) to prevent this from making the result longer than two :)
-    $('#timerValue').html(('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2));
-    console.log(t);
+    $('#timerValue').html(('0' + t.minutes).slice(-2) + ':' + ('0' + t.seconds).slice(-2))
+    console.log(t)
 
     // If no more time left, use clearInterval to terminate the timer, alert the user, and start
     // the next break or work session as appropriate
     if (t.total <= 0) {
-      clearInterval(interval);
+      clearInterval(interval)
       if (type === 'break') {
-        alert("Your break is over, let's get started on the next " + pomtime + " minute(s) of awesome work!");
-        startPomodoro();
+        alert('Your break is over, let\'s get started on the next ' + pomtime + ' minute(s) of awesome work!')
+        startPomodoro()
       } else {
-        alert("Great job! Time to take a well-earned break!");
-        startBreak();
+        alert('Great job! Time to take a well-earned break!')
+        startBreak()
       }
     }
-  }, 1000);
+  }, 1000)
 }
 
 // Begins countdown from 25 minutes by default, or new user-defined value
-function startPomodoro() {
+function startPomodoro () {
   // Stop any existing interval function
-  clearInterval(interval);
+  clearInterval(interval)
   // Reset the timer text
-  $('#timerValue').html(('0' + pomtime).slice(-2) + ':00');
+  $('#timerValue').html(('0' + pomtime).slice(-2) + ':00')
   // Create a new timer for current time + 25 minutes in ms (25 min * 60 sec/min * 1000 sec/ms)
   // This can be edited by the user clicking the +/- buttons
-  var newPomodoro = new Date(Date.parse(new Date()) + pomtime * 60 * 1000);
-  timerRunning = true;
-  startClock(newPomodoro, 'pomodoro');
+  var newPomodoro = new Date(Date.parse(new Date()) + pomtime * 60 * 1000)
+  timerRunning = true
+  startClock(newPomodoro, 'pomodoro')
 }
 
 // Begins break countdown from 5 minutes.
-function startBreak() {
+function startBreak () {
   // Stop the interval function
-  clearInterval(interval);
+  clearInterval(interval)
   // Reset the timer text
-  $('#timerValue').html('05:00');
+  $('#timerValue').html('05:00')
   // Create a new break timer for current time + 5 minutes in ms (5 min * 60 sec/min * 1000 sec/ms)
-  var newBreakTimer = new Date(Date.parse(new Date()) + 5 * 60 * 1000);
-  timerRunning = true;
-  startClock(newBreakTimer, 'break');
+  var newBreakTimer = new Date(Date.parse(new Date()) + 5 * 60 * 1000)
+  timerRunning = true
+  startClock(newBreakTimer, 'break')
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   // Start a new pomodoro timer
-  $('#startTimer').click(function() {
-    console.log("Beginning new timer...");
-    startPomodoro();
-  });
+  $('#startTimer').click(function () {
+    console.log("Beginning new timer...")
+    startPomodoro()
+  })
 
   // Start a break
-  $('#breakStart').click(function() {
-    console.log("Taking a break...");
-    startBreak();
-  });
+  $('#breakStart').click(function () {
+    console.log('Taking a break...')
+    startBreak()
+  })
 
   // Reset timer to whatever the default pomodoro has been set to
-  $('#resetTimer').click(function() {
+  $('#resetTimer').click(function () {
     // Stop the interval function
-    clearInterval(interval);
-    console.log("Resetting");
+    clearInterval(interval)
+    console.log('Resetting')
     // Reset the timer text
-    $('#timerValue').html(('0' + pomtime).slice(-2) + ':00');
-    timerRunning = false;
-  });
+    $('#timerValue').html(('0' + pomtime).slice(-2) + ':00')
+    timerRunning = false
+  })
 
   // Increase default pomodoro time by 1 minute
-  $('#addMin').click(function() {
-    pomtime++;
-    console.log("Incrementing pomodoro length to: " + pomtime);
+  $('#addMin').click(function () {
+    pomtime++
+    console.log('Incrementing pomodoro length to: ' + pomtime)
     // Remember to also update the display!
     if (!timerRunning) {
-      $('#timerValue').html(('0' + pomtime).slice(-2) + ':00');
+      $('#timerValue').html(('0' + pomtime).slice(-2) + ':00')
     }
-  });
+  })
 
   // Reduce default pomodoro time by 1 minute if reasonable
-  $('#subMin').click(function() {
+  $('#subMin').click(function () {
     if (pomtime <= 1) {
-      alert("Minimum timer length is 1 minute, smart move to try working in 0 minute incremements, though!");
+      alert('Minimum timer length is 1 minute, smart move to try working in 0 minute incremements, though!')
     } else {
-      pomtime--;
-      console.log("Decrementing pomodoro length to: " + pomtime);
+      pomtime--
+      console.log('Decrementing pomodoro length to: ' + pomtime)
       // Remember to also update the display!
       if (!timerRunning) {
-        $('#timerValue').html(('0' + pomtime).slice(-2) + ':00');
+        $('#timerValue').html(('0' + pomtime).slice(-2) + ':00')
       }
     }
-  });
-
-});
+  })
+})
